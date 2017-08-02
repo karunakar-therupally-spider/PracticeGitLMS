3783c6f97583656fede35ed2c6ca05120963c2c3
karunakar-therupally-spider <ktherupally@spiderlogic.com>
2017-08-02 13:13:06 +0530

    Removed unwanted packages Tasks and Linq

--- MyFirstBDD/Settings/ObjectRepository.cs
+++ MyFirstBDD/Settings/ObjectRepository.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using OpenQA.Selenium;
 //using LMSUI.Interfaces;
 //using LMSUI.PageObject;
