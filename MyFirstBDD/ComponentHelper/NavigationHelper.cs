277dd79b210677cc1a49efde4b704a2351112875
karunakar-therupally-spider <ktherupally@spiderlogic.com>
2017-08-02 13:12:32 +0530

    Removed unwanted packages Tasks and Linq

--- MyFirstBDD/ComponentHelper/NavigationHelper.cs
+++ MyFirstBDD/ComponentHelper/NavigationHelper.cs
@@ -1,9 +1,7 @@
 ﻿using LMSUI.Settings;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace LMSUI.ComponentHelper
 {
